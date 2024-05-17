4f6950c0501810579b6e692ae935d784f18ea403
uJoeLu <joel.neves37@gmail.com>
2024-05-17 19:57:45 -0300

    correções

--- HashNode.java
+++ HashNode.java
@@ -1,5 +1,3 @@
-import java.util.LinkedList;
-
 class HashNode<K, V> {
     K key;
     V value;
@@ -9,59 +7,5 @@ class HashNode<K, V> {
         this.key = key;
         this.value = value;
         this.next = null;
-    }
-}
-
-public class HashTable<K, V> {
-    private static final int SIZE = 16;
-    private LinkedList<HashNode<K, V>>[] buckets;
-
-    public HashTable() {
-        buckets = new LinkedList[SIZE];
-        for (int i = 0; i < SIZE; i++) {
-            buckets[i] = new LinkedList<>();
-        }
-    }
-
-    private int getHash(K key) {
-        return Math.abs(key.hashCode() % SIZE);
-    }
-
-    public void put(K key, V value) {
-        int index = getHash(key);
-        LinkedList<HashNode<K, V>> bucket = buckets[index];
-        for (HashNode<K, V> node : bucket) {
-            if (node.key.equals(key)) {
-                node.value = value;
-                return;
-            }
-        }
-        bucket.add(new HashNode<>(key, value));
-    }
-
-    public V get(K key) {
-        int index = getHash(key);
-        LinkedList<HashNode<K, V>> bucket = buckets[index];
-        for (HashNode<K, V> node : bucket) {
-            if (node.key.equals(key)) {
-                return node.value;
-            }
-        }
-        return null;
-    }
-
-    public void remove(K key) {
-        int index = getHash(key);
-        LinkedList<HashNode<K, V>> bucket = buckets[index];
-        HashNode<K, V> toRemove = null;
-        for (HashNode<K, V> node : bucket) {
-            if (node.key.equals(key)) {
-                toRemove = node;
-                break;
-            }
-        }
-        if (toRemove != null) {
-            bucket.remove(toRemove);
-        }
     }
 }
---
+++ HashTable.java
@@ -0,0 +1,55 @@
+import java.util.LinkedList;
+
+public class HashTable<K, V> {
+    private static final int size = 16;
+    private LinkedList<HashNode<K, V>>[] buckets;
+
+    public HashTable() {
+        buckets = new LinkedList[size];
+        for (int i = 0; i < size; i++) {
+            buckets[i] = new LinkedList<>();
+        }
+    }
+
+    private int getHash(K key) {
+        return Math.abs(key.hashCode() % size);
+    }
+
+    public void put(K key, V value) {
+        int index = getHash(key);
+        LinkedList<HashNode<K, V>> bucket = buckets[index];
+        for (HashNode<K, V> node : bucket) {
+            if (node.key.equals(key)) {
+                node.value = value;
+                return;
+            }
+        }
+        bucket.add(new HashNode<>(key, value));
+    }
+
+    public V get(K key) {
+        int index = getHash(key);
+        LinkedList<HashNode<K, V>> bucket = buckets[index];
+        for (HashNode<K, V> node : bucket) {
+            if (node.key.equals(key)) {
+                return node.value;
+            }
+        }
+        return null;
+    }
+
+    public void remove(K key) {
+        int index = getHash(key);
+        LinkedList<HashNode<K, V>> bucket = buckets[index];
+        HashNode<K, V> toRemove = null;
+        for (HashNode<K, V> node : bucket) {
+            if (node.key.equals(key)) {
+                toRemove = node;
+                break;
+            }
+        }
+        if (toRemove != null) {
+            bucket.remove(toRemove);
+        }
+    }
+}
